a2918a9b733df1f967aa43aac911d019cf33e2bb
hvnwntw8 <ryan.m.lee89@gmail.com>
2020-04-07 10:05:37 -0700

    grabbing userid into url

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,20 +1,70 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import { signout, isAuthenticated } from '../auth';
 
-const Menu = () => (
-  <div>
-    <ul className="nav nav-tabs bg-primary">
-      <li className="nav-item">
-        <Link className="nav-link" to="/">Home</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/signin">Sign In</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/signup">Sign Up</Link>
-      </li>
-    </ul>
-  </div>
-)
+const isActive = (history, path) => {
+    if (history.location.pathname === path) return { color: '#ff9900' };
+    else return { color: '#ffffff' };
+};
 
-export default Menu;
+const Menu = ({ history }) => (
+    <div>
+        <ul className="nav nav-tabs bg-primary">
+            <li className="nav-item">
+                <Link className="nav-link" style={isActive(history, '/')} to="/">
+                    Home
+                </Link>
+            </li>
+
+            {!isAuthenticated() && (
+                <React.Fragment>
+                    <li className="nav-item">
+                        <Link className="nav-link" style={isActive(history, '/signin')} to="/signin">
+                            Sign In
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" style={isActive(history, '/signup')} to="/signup">
+                            Sign Up
+                        </Link>
+                    </li>
+                </React.Fragment>
+            )}
+
+            {isAuthenticated() && isAuthenticated().user.role === 'admin' && (
+                <li className="nav-item">
+                    <Link to={`/admin`} style={isActive(history, `/admin`)} className="nav-link">
+                        Admin
+                    </Link>
+                </li>
+            )}
+
+            {isAuthenticated() && (
+                <React.Fragment>
+
+                    <li className="nav-item">
+                        <Link
+                            to={`/user/${isAuthenticated().user._id}`}
+                            style={isActive(history, `/user/${isAuthenticated().user._id}`)}
+                            className="nav-link"
+                        >
+                            {`${isAuthenticated().user.name}'s profile`}
+                        </Link>
+                    </li>
+
+                    <li className="nav-item">
+                        <span
+                            className="nav-link"
+                            style={{ cursor: 'pointer', color: '#fff' }}
+                            onClick={() => signout(() => history.push('/'))}
+                        >
+                            Sign Out
+                        </span>
+                    </li>
+                </React.Fragment>
+            )}
+        </ul>
+    </div>
+);
+
+export default withRouter(Menu);
