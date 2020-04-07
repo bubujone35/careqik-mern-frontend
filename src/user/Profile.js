999e34a26f530900957c5d5af620f1dc8e8419a3
hvnwntw8 <ryan.m.lee89@gmail.com>
2020-04-07 13:11:11 -0700

    extracted read method to its own file

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { isAuthenticated } from '../auth';
+import { Redirect } from 'react-router-dom';
+import { read } from './apiUser';
 
 class Profile extends Component {
   constructor() {
@@ -10,34 +12,33 @@ class Profile extends Component {
     }
   }
 
+
+  init = userId => {
+    const token = isAuthenticated().token
+      read(userId, token)
+      .then(data => {
+        if(data.error) {
+          this.setState({redirectToSignin: true})
+        } else {
+          this.setState({user: data})
+        }
+      });
+    };
+
   componentDidMount() {
-    const userId = this.props.match.params.userId
-    fetch(`${process.env.REACT_APP_API_URL}/user/${userId}`, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        "Content-type": "application/json",
-        Authorization: `Bearer ${isAuthenticated().token}`
-      }
-    })
-    .then(response => {
-      return response.json()
-    })
-    .then(data => {
-      if(data.error) {
-        console.log("ERROR")
-      } else {
-        this.setState({user: data})
-      }
-    })
+    const userId = this.props.match.params.userId;
+    this.init(userId);
   }
 
   render() {
+    const redirectToSignin = this.state.redirectToSignin
+    if(redirectToSignin) return <Redirect to="/signin"/>
     return (
       <div className="container">
         <h2 className="mt-5 mb-5">Profile</h2>
         <p>Hello, {isAuthenticated().user.name}</p>
         <p>Email: {isAuthenticated().user.email}</p>
+        <p>{`Profile created ${new Date(this.state.user.created).toDateString()}`}</p>
       </div>
     );
   }
